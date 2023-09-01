339603bda7d16012bd3e5a789c9d92b018bf06ff
iuryuri <129408028+iuryuri@users.noreply.github.com>
2023-09-01 09:34:44 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+//atribui o valor da tag h1 para a variavel titulo
+let titulo = document.querySelector("h1").textContent;
+//mostra no console do navegador a variavel titulo
+console.log(titulo);
+//modifica o valor do titulo para frase "Fica grandão biiiil"
+titulo.textContent = "Fica grandão biiiil";
+
+//---------------aqui vamos mexer na tabela de imc-----
+let paciente = document .querySelector("#primeiro-paciente");
+console.log(paciente);
