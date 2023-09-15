3e32b056c942ba91eb4823c876d642b970e28b07
iuryuri <129408028+iuryuri@users.noreply.github.com>
2023-09-15 10:47:49 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -6,11 +6,23 @@ console.log(titulo);
 document.querySelector("h1").textContent = "Fica grandão birllll";
 
 //---------------aqui vamos mexer na tabela de imc-----
-// armazena na
-let paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente);
+// armazena no ARRAY paciente as informações de toos os pacientes//
+let pacientes = document.querySelectorAll(".paciente");
+//console.log(paciente);
+
+//loop for para percorrer o array//
+for(let i = 0 ; i < pacientes.length; i++){
+    let paciente = pacientes[i];
+    console.log(paciente);
+    
+
+      
+
+
+//armazenamento somente as colunas de peso e altura
  let tdPeso = paciente.querySelector(".info-peso");
-  let tdAltura = paciente.querySelector(".info-altura");
+ let tdAltura = paciente.querySelector(".info-altura");
+
 //armazena somente as informaçãoes de teto de peso e a altura//
 let peso = tdPeso.textContent;
 let altura = tdAltura.textContent;
@@ -26,21 +38,23 @@ let pesoEhValido = true;
 let alteraEhValido = true;
 
 if(peso < 0 || peso > 1000){
-    console.log("Peso inválido!");
-    tdImc.textContent = "Peso Invalido Faça Uma Dieta "
-    pesoEhValido = false;
+   console.log("Peso inválido!");
+   tdImc.textContent = "Peso Invalido Faça Uma Dieta "
+   pesoEhValido = false;
 }
-    
+   
 if(altura < 0 || altura > 3){
-    console.log(" altura inválido!");
-    tdImc.textContent = " Altura Invalido ou ta grande de mais ou pequeno use gh "
-    alteraEhValido = false;
+   console.log(" altura inválido!");
+   tdImc.textContent = " Altura Invalido ou ta grande de mais ou pequeno use gh "
+   alteraEhValido = false;
 }
 if(pesoEhValido === true && alteraEhValido=== true){
-    // realizar o calculo do imc
-    let imc = peso / (altura * altura);
-    console.log(imc);
-    tdImc.textContent = imc;
+   // realizar o calculo do imc
+   let imc = peso / (altura * altura);
+   console.log(imc);
+   tdImc.textContent = imc.toFixed(2);
+   
 } else{
-      tdImc.textContent = "Peso e/ou altura invalido ";
+     tdImc.textContent = "Peso e/ou altura invalido ";
+}
 }
